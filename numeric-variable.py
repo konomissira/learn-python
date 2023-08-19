3fb7e3547465116d1a5efe1ba30054c40adffbc9
N2SuccessAll <camaram9@lsbu.ac.uk>
2023-08-19 11:26:02 +0100

    Add numeric-variable.py file

---
+++ numeric-variable.py
@@ -0,0 +1,10 @@
+"""
+rectangle_width = 15;
+rectangle_length = 5;
+area_of_rectangle = rectangle_width * rectangle_length;
+print("Area of rectangle is " + str(area_of_rectangle));
+"""
+print("Type a number here: ");
+first_number = input();
+add_ten = str(first_number) + 10;
+print(add_ten);
