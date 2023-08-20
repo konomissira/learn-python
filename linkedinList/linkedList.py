dc36bcb014d3e01f0026cf76a9d3d61c338ed084
N2SuccessAll <camaram9@lsbu.ac.uk>
2023-08-20 22:26:42 +0100

    add files linkedList.py

---
+++ linkedinList/linkedList.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__(self, value):
+        self.value  = value
+        self.next = None
+
+def find(node, element):
+    while node:
+        if(node.value == element):
+            return True
+        node = node.next
+    return False;
+
+if __name__ == '__main__':
+    head = Node(1)
+    head.next = Node(2)
+    head.next.next = Node(3)
+    head.next.next.next = Node(4)
+    head.next.next.next.next = Node(5)
+    
+
+    print(find(head, 7))
+    print(find(head, 6))
