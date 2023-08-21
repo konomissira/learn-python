289ef9df5d1894db0d83b0ae5900d5830dd6be8e
N2SuccessAll <camaram9@lsbu.ac.uk>
2023-08-21 15:05:00 +0100

    Make calculations in Python such as converting temperature in Fahrenheit to Celsius. Python has dinamic typing which mean variables don't have types.

---
+++ calculations.py
@@ -0,0 +1,5 @@
+name = input("What is your name: ")
+age = input("How old are you ? ")
+age_want_to_reach = 100 - int(age)
+
+print("Hello " + name + " you need " + str(age_want_to_reach)+ " " + "more years to get 100 years")
---
+++ converting_temperatures.py
@@ -0,0 +1,4 @@
+temp_fahrenheit = input("Enter the temperature in fahrnheit: ")
+temp_celcius = (float(temp_fahrenheit) - 32) * 0.5556
+
+print("The temperature is: " + str(temp_celcius) + " degree celsius.")
