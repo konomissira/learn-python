92a3b7a870427bb35ea165ffafd2728964e9b2f0
N2SuccessAll <mahamadtoumani@gmail.com>
2023-09-08 22:31:29 +0100

    Create a function that ask a user to enter his weight in kilos, and his height in metres, then calculate his BMI. Understand the basic of tuples in Python.

---
+++ containers/tuples.py
@@ -0,0 +1,14 @@
+def main():
+  programming_languages = ("C", "C++", "Java", "Python", "Go", "Perl")
+
+  print(programming_languages) 
+  print(programming_languages[0]) 
+  print(programming_languages[4])
+
+  length = len(programming_languages)
+  print(length)
+
+  for language in programming_languages:
+    print(language)
+
+main()
